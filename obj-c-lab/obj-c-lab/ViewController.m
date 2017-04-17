f4e18b6a1c7a50d656d1ee52e83178ad66876c97
Brandon Little <brlittle86@gmail.com>
2017-04-17 14:24:08 -0700

    implemented NSString category with method for reversal of a string.

--- obj-c-lab/obj-c-lab/ViewController.m
+++ obj-c-lab/obj-c-lab/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "NSString+String.h"
 
 @interface ViewController ()
 
@@ -16,13 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSString *sampleString = @"I like potatoes.";
+    
+    [sampleString reversedString];
+    
 }
 
 
