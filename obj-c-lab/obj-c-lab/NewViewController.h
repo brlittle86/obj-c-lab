0c11d8fde4c578b0a043d5498a3fc37b96e89bfe
Brandon Little <brlittle86@gmail.com>
2017-04-18 15:44:56 -0700

    created tableView and linked into ViewController file

---
+++ obj-c-lab/obj-c-lab/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  obj-c-lab
+//
+//  Created by Brandon Little on 4/18/17.
+//  Copyright © 2017 Brandon Little. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
