0bb99a9f20c3039a699815fffc57e2439c699fb1
Brandon Little <brlittle86@gmail.com>
2017-04-20 15:07:15 -0700

    implemented MRC for Employee class

--- obj-c-lab/obj-c-lab/Employee.m
+++ obj-c-lab/obj-c-lab/Employee.m
@@ -93,15 +93,36 @@ NSString *_managerName;
 
 //Setter
 -(void)setEmployeeNumber:(NSNumber *)employeeNumber {
-    _employeeNumber = employeeNumber;
+    
+    if (_employeeNumber != employeeNumber) {
+        
+        [employeeNumber retain];
+        [_employeeNumber release];
+        
+        _employeeNumber = employeeNumber;
+    }
 }
 
 -(void)setYearsEmployed:(NSNumber *)yearsEmployed {
-    _yearsEmployed = yearsEmployed;
+    
+    if (_yearsEmployed != yearsEmployed) {
+        
+        [yearsEmployed retain];
+        [_yearsEmployed release];
+        
+        _yearsEmployed = yearsEmployed;
+    }
 }
 
 -(void)setManagerName:(NSString *)managerName {
-    _managerName = managerName;
+    
+    if (_managerName != managerName) {
+        
+        [managerName retain];
+        [_managerName release];
+        
+        _managerName = managerName;
+    }
 }
 
 @end
